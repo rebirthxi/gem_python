b88fa56216a9ae07927762bf12879085850c01cb
Daniel R. Koris <korisd@gmail.com>
2020-11-08 10:12:35 -0700

    fixup into above

--- src/gem_python/packets/ver_check_packet.py
+++ src/gem_python/packets/ver_check_packet.py
@@ -7,8 +7,6 @@ class VerCheckResponse(BasicPacket):
         self.version = ""
         self.expansion_mask = 0xFFE
         self.feature_mask = 0xD
-        # self.expansion_mask = 0
-        # self.feature_mask = 0
 
     def to_bytes(self) -> bytes:
         data = bytearray(
@@ -20,15 +18,9 @@ class VerCheckResponse(BasicPacket):
         data[32:34] = int(self.expansion_mask).to_bytes(2, byteorder="little")
         data[36:38] = int(self.feature_mask).to_bytes(2, byteorder="little")
 
-        for x in range(len(data)):
-            print("{} {}".format(x, hex(data[x])))
-
         # md5 checksum
         data[12:28] = hashlib.md5(data).digest()
-        for x in hashlib.md5(data).digest():
-            print(hex(x))
-        for x in range(len(data)):
-            print("{} {}".format(x, hex(data[x])))
+
         return data
 
     def from_bytes(self, data: bytes) -> None:
